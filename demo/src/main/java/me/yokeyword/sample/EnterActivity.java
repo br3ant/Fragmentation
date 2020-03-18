c43afc5caa246905e612282194b80e62f7290010
br3ant <1106617567@qq.com>
2020-03-18 10:29:27 +0800

    删除除核心代码外的所有代码

--- demo/src/main/java/me/yokeyword/sample/EnterActivity.java
+++ demo/src/main/java/me/yokeyword/sample/EnterActivity.java
@@ -1,13 +1,11 @@
 package me.yokeyword.sample;
 
-import android.content.Intent;
 import android.os.Bundle;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import android.view.View;
 
-import me.yokeyword.sample.demo_flow.MainActivity;
 
 /**
  * Created by YoKeyword on 16/6/5.
@@ -25,26 +23,5 @@ public class EnterActivity extends AppCompatActivity {
     private void initView() {
         Toolbar toolBar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolBar);
-
-        findViewById(R.id.btn_flow).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(EnterActivity.this, MainActivity.class));
-            }
-        });
-
-        findViewById(R.id.btn_wechat).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(EnterActivity.this, me.yokeyword.sample.demo_wechat.MainActivity.class));
-            }
-        });
-
-        findViewById(R.id.btn_zhihu).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(EnterActivity.this, me.yokeyword.sample.demo_zhihu.MainActivity.class));
-            }
-        });
     }
 }
